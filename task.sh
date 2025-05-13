c480c95c2f1e9dfdf1f06a50f3c580e8e0586f77
mikic202 <mikolaj.chomanski@gmail.com>
2025-05-13 20:40:22 +0200

    Add code for the Athena

---
+++ task.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+module load GCCcore/12.3.0 Python/3.11.3
+module load GCC/11.3.0  OpenMPI/4.1.4 PyTorch/1.13.1-CUDA-11.7.0
+# module load GCC/12.3.0 matplotlib/3.7.2
+
+echo "CUDA visible devices: $CUDA_VISIBLE_DEVICES"
+
+INDEX=${SLURM_ARRAY_TASK_ID:-0}
+
+mkdir -p task_$INDEX/models
+
+python3 main.py $INDEX
